a6a992d4bdbbb339b956e24703e4d2e3b22673ac
Iona Grant <ionagrant92@gmail.com>
2019-10-14 20:12:24 +0100

    initial commit

---
+++ who_dunnit.js
@@ -0,0 +1,30 @@
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+//this should run properly because it isn't trying to access information that is inside the constant, it is running it to get the information.
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+//this won't work because it is trying to change a constant which is immutable.
